e4f46eaee61c715c1ba8a7f5f8121c4eaae07105
Ogreaggressive <nicolascari.rz@gmail.com>
2023-09-09 20:09:12 -0400

    creando pagina de nuevoViaje

---
+++ src/pages/nuevoViaje.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopNavigationBar from '../components/menu/topNavigationBar'
+
+const NuevoViaje = () => {
+  return (
+    <div>
+      nuevo viaje
+    </div>
+  );
+}
+
+export default NuevoViaje;
--- src/router.jsx
+++ src/router.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ViajesEnCurso from './pages/viajesEnCurso';
 import Estadisticas from './pages/Estadisticas';
 import Inventario from './pages/Inventario';
+import NuevoViaje from './pages/nuevoViaje';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path='/viajesEnCurso' element={<ViajesEnCurso/>} />
-          <Route path='/inventario' element={<Inventario/>} />
-          <Route path='/estadisticas' element={<Estadisticas/>} />
+        <Route path='/inventario' element={<Inventario/>} />
+        <Route path='/estadisticas' element={<Estadisticas/>} />
+        <Route path='/nuevoViaje' element={<NuevoViaje/>} />
         <Route path='/' element={<Estadisticas/>} />
       </Routes>
     </Router>
